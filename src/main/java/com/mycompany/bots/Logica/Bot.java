355d436e9bc1111ee2a32166344e60c0bd631244
Juan Jesus Mata Solis <Hydratec9@outlook.com>
2020-02-15 16:00:53 -0700

    Ya va quedando esta cosa

---
+++ src/main/java/com/mycompany/bots/Logica/Bot.java
@@ -0,0 +1,90 @@
+package com.mycompany.bots.Logica;
+
+import java.util.ArrayList;
+
+/**
+ * Clase principal encargada de crear los bots
+ */
+public class Bot {
+    private ArrayList<String> respuestas;
+    private String nombreBot, nombreUsuario;
+
+    public Bot(ArrayList<String> respuestas, String nombreBot) {
+        this.respuestas = respuestas;
+        this.nombreBot = nombreBot;
+    }
+
+    /**
+     * Una funcion simple para iniciar la conversacion
+     *
+     * @return Presentacion del bot
+     */
+    public String encender() {
+        return "Hola yo soy " + nombreBot + " un gusto saludarte, sobre que te gustaria hablar?";
+    }
+
+    /**
+     * Funcion principal que ejercera el bot
+     *
+     * @param mensaje solicitud del usuario
+     * @return un mensaje para continuar la platica
+     */
+    public String hablar(String mensaje) {
+        switch (evaluar(mensaje)) {
+            case 0:
+                return "Hola " + nombreUsuario + " un gusto saber de ti";
+            case 1:
+                return charlaRan();
+            case 2:
+                return "Que tengas un gran día " + nombreUsuario;
+            default:
+                return "Lo siento no se que tratas de decir";
+        }
+    }
+
+    /**
+     * Funcion para generar un poco de charla random
+     *
+     * @return una respuesta aleatoria
+     */
+    private String charlaRan() {
+        return respuestas.get((int) (Math.random() * respuestas.size()));
+    }
+
+    /**
+     * Evalua el tipo de mensaje que mando el usuario
+     *
+     * @param mensaje mensaje mandado por el usuario
+     * @return Regresa una respuesta posible a lo que el usuario introdujo
+     */
+    private int evaluar(String mensaje) {
+        if (mensaje.toLowerCase().contains("hola") || mensaje.toLowerCase().contains("mucho gusto") || mensaje.toLowerCase().contains("saludos")) {
+            return 0;
+        } else if (mensaje.toLowerCase().contains("adios") || mensaje.toLowerCase().contains("fue un gusto") || mensaje.toLowerCase().contains("hasta luego")) {
+            return 2;
+        } else if (mensaje.toLowerCase().contains("como estas") || mensaje.toLowerCase().contains("que dices") || mensaje.toLowerCase().contains("habla")) {
+            return 1;
+        } else {
+            return 3;
+        }
+    }
+
+
+    /**
+     * Funcion para conocer el nombre del bot
+     *
+     * @return regresa el nombre del bot
+     */
+    public String getNombreBot() {
+        return nombreBot;
+    }
+
+    /**
+     * establece eñ nombre del usuario
+     *
+     * @param nombreUsuario nombre con el que el bot se referira al usuario
+     */
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+}
